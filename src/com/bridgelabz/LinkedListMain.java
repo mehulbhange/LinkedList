d19c8687c4d60717e024f95ebaf1cdca995d2ac4
mehulbhange <bhangemehul@gmail.com>
2022-08-27 10:33:42 +0530

    ADDED:[Mehul] added welcome message

---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+/**
+ * Purpose - Linked list Operations
+ * @author - Mehul Bhange
+ * @since - 27-08-2022
+ */
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to linked list problem");
+    }
+}
